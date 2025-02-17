01df848f2169406ddc351a16caeb05fa2ad66b7e
alessandroschiano98 <schianoski98@gmail.com>
2025-02-17 13:07:08 +0100

    raccolta dati

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*
+RACCOLTA DATI:
+
+-1)Chiedere all'utente il numero di chilometri che vuole percorrere; (FATTO)
+-2)Chiedere all'utente l'età;(FATTO)
+
+// CONSIDERAZIONI: 
+- prezzo del biglietto costa 0.21€ al KM
+- 20% sconto per i <18 (minorenni)
+- 40% per >65 (over65)
+
+ESECUZIONE LOGICA:
+- Se l'utente che acquista il biglietto è <18 (minorenne) paga 0.21€ - il 20% (calcolo: ticket x userKm / 100)
+
+
+*/
+
+// NOME E COGNOME //
+const nameSurname = prompt ("Benvenuto in Boolean, mi scrivi il tuo Nome e Cognome?")
+console.log (nameSurname);
+
+// KM DA PERCORRERE // 
+const userKmStr = prompt ("Quanti chilometri vuoi percorrere?")
+const userKm = parseInt (userKmStr)
+console.log (userKm);
+
+// ETA' DEL'UTENTE // 
+const userAgeStr = prompt ("Mi ricordi quanti anni hai?")
+const userAge = parseInt (userAgeStr)
+console.log(userAge);
+
+
+// ESECUZIONE LOGICA // 
