20de48a1a0f26b6bfa03301f893cc70fa94f5553
alessandroschiano98 <schianoski98@gmail.com>
2025-02-17 22:35:10 +0100

    userAge da correggere

--- js/script.js
+++ js/script.js
@@ -9,29 +9,67 @@ RACCOLTA DATI:
 - 20% sconto per i <18 (minorenni)
 - 40% per >65 (over65)
 
-ESECUZIONE LOGICA:
-- Se l'utente che acquista il biglietto è <18 (minorenne) paga 0.21€ - il 20% (calcolo: ticket x userKm / 100)
+- (calcolo: ticket x userKm / 100)
 
 
 */
 
 // KM DA PERCORRERE // 
-const userKmStr = prompt ("Quanti chilometri vuoi percorrere?")
-const userKm = parseInt (userKmStr)
-console.log (userKm);
+const userKmStr = prompt("Quanti chilometri vuoi percorrere?")
+const userKm = parseInt(userKmStr)
+console.log(userKm);
 
 // ETA' DEL'UTENTE // 
-const userAgeStr = prompt ("Mi ricordi quanti anni hai?")
-const userAge = parseInt (userAgeStr)
+const userAgeStr = prompt("Mi ricordi quanti anni hai?")
+const userAge = parseInt(userAgeStr)
 console.log(userAge);
+
+// MINORENNE / OVER65 // 
+
+const  userMinorenneStr = prompt (userAge < 18)
+const userMinorenne = parseInt (userMinorenneStr)
+
+console.log(userMinorenne);
+
+const  userMaggiorenneStr = prompt (userAge > 18)
+const userMaggiorenne = parseInt (userMaggiorenneStr)
+console.log(userMaggiorenne);
+
+const  userUnder65Str = prompt (userAge < 65)
+const userUnder65 = parseInt (userUnder65Str)
+console.log(userUnder65);
+
+const userOver65Str = prompt (userAge > 65)
+const userOver65 = parseInt (userOver65Str)
+console.log (userOver65);
+
 
 
 // ESECUZIONE LOGICA // 
+
+// BIGLIETTO //
 const ticket = (0.21 * userKm)
 console.log(ticket);
 
-const saleTicket = (ticket * 80 ) / 100;
+// SCONTO BIGLIETTO // 
+const saleTicket = (ticket * 80) / 100;
 console.log(saleTicket);
+
+// MINORENNE / OVER65 // 
+
+if (userMinorenne) {
+    message: "Hai diritto al 20% di sconto";
+   }
+else if (userMaggiorenne, userUnder65) {
+    message: "Non hai diritto a nessuno sconto";
+}
+else (userOver65) {
+    message "Hai diritto al 40% di sconto";
+}
+
+console.log(userMinorenne, userMaggiorenne, userUnder65, userOver65);
+
+
 
 
 
