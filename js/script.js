7a569c9069d3b049892ef02eb0d38538dbe4cc8e
alessandroschiano98 <schianoski98@gmail.com>
2025-02-18 17:04:09 +0100

    esercizio da completare

--- js/script.js
+++ js/script.js
@@ -19,33 +19,16 @@ const userKmStr = prompt("Quanti chilometri vuoi percorrere?")
 const userKm = parseInt(userKmStr)
 console.log(userKm);
 
+
 // ETA' DEL'UTENTE // 
 const userAgeStr = prompt("Mi ricordi quanti anni hai?")
 const userAge = parseInt(userAgeStr)
 console.log(userAge);
 
-// MINORENNE / OVER65 // 
-
-const  userMinorenneStr = prompt (userAge < 18)
-const userMinorenne = parseInt (userMinorenneStr)
-
-console.log(userMinorenne);
-
-const  userMaggiorenneStr = prompt (userAge > 18)
-const userMaggiorenne = parseInt (userMaggiorenneStr)
-console.log(userMaggiorenne);
-
-const  userUnder65Str = prompt (userAge < 65)
-const userUnder65 = parseInt (userUnder65Str)
-console.log(userUnder65);
-
-const userOver65Str = prompt (userAge > 65)
-const userOver65 = parseInt (userOver65Str)
-console.log (userOver65);
-
-
 
 // ESECUZIONE LOGICA // 
+
+
 
 // BIGLIETTO //
 const ticket = (0.21 * userKm)
@@ -55,19 +38,7 @@ console.log(ticket);
 const saleTicket = (ticket * 80) / 100;
 console.log(saleTicket);
 
-// MINORENNE / OVER65 // 
 
-if (userMinorenne) {
-    message: "Hai diritto al 20% di sconto";
-   }
-else if (userMaggiorenne, userUnder65) {
-    message: "Non hai diritto a nessuno sconto";
-}
-else (userOver65) {
-    message "Hai diritto al 40% di sconto";
-}
-
-console.log(userMinorenne, userMaggiorenne, userUnder65, userOver65);
 
 
 
